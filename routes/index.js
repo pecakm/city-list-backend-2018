a1eca2bbe7962f3469503ace5a19a932507702aa
Mikołaj Pęcak <mikolaj@moodup.team>
2018-07-07 19:17:17 +0200

    Send cities from controller to view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+let cities = ['Leszno', 'Poznań', 'Kalisz'];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { cities: cities });
 });
 
 module.exports = router;
