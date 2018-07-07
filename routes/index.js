04058eb9eb15c7eda95b0e86f5b3a5563af5ea6b
Mikołaj Pęcak <mikolaj@moodup.team>
2018-07-07 18:21:45 +0200

    Add some random test

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,12 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+let convert = {};
+
+convert.fToC = function(fahrenheit) {
+    if(!Number.isInteger(fahrenheit)) return undefined;
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+module.exports = convert;
---
+++ test/test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var converter = require('../routes/index');
+
+describe('Temperature Conversion', function() {
+    describe('fToC', function() {
+        it('should convert -40 fahrenheit to -40 celsius', function() {
+            assert.equal(-40, converter.fToC(-40));
+        });
+        it('should convert 32 fahrenheit to 0 celsius', function() {
+            assert.equal(0, converter.fToC(32));
+        });
+        it('should return undefined if no temperature is input', function(){
+            assert.equal(undefined, converter.fToC(''));
+        });
+    });
+});
