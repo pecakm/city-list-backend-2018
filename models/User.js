dd695be40270696fc604bc48029a46b5e93be90b
Mikołaj Pęcak <mikolaj@moodup.team>
2018-07-15 21:28:25 +0200

    Create User model

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('../db/connection');
+
+var userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    passwordConf: {
+        type: String,
+        required: true,
+    }
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
